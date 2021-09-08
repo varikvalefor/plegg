2a8df39f5b04b5c15f46a066dd54acef920d63d6
Varik Valefor <varikvalefor@aol.com>
2021-09-08 16:11:34 -0400

    src/Plegg.hs: unveil(2) support is added.

--- src/Plegg.hs
+++ src/Plegg.hs
@@ -8,8 +8,8 @@
 -- Maintainer  : varikvalefor@aol.com
 -- Stability   : experimental
 -- Portability : portable
--- This module contains @'plegg'@, a high-level interface for OpenBSD's
--- @pledge(2)@.
+-- This module contains high-level interfaces for OpenBSD's @pledge(2)@ and
+-- @unveil(2)@.
 module Plegg where
 
 #ifdef openbsd_HOST_OS
@@ -19,6 +19,7 @@ module Plegg where
   import System.Directory;
 
   foreign import ccall "unistd.h pledge" pledge :: CString -> Ptr [CString] -> IO Int;
+  foreign import ccall "unistd.h unveil" unveil :: CString -> CString -> IO Int;
 
   -- | @plegg k@ is equivalent to C's "@pledge(k, nulll)@".
   --
@@ -30,9 +31,29 @@ module Plegg where
   plegg k = throwErrnoIfMinus1_ "pledge fails!" $
             withCString k $ \premises ->
             pledge premises nullPtr;
+
+  -- | @univac@ is a high-level interface for @unveil(2)@.
+  --
+  -- For all @(a,b)@ in @g@, @univac g@ runs @unveil(a, b)@.
+  univac :: [(String, String)]
+         -- ^ [(PATH WHICH SHOULD BE EXPOSED, PERMISSIONS)]
+         -> IO ();
+  univac = mapM_ expose
+    where
+    expose :: (String, String) -> IO ()
+    expose (path, perms) =
+      throwErrnoIfMinus1_ "unveil hath fallen!" $
+      withCString path $ \pathC ->
+      withCString perms $ \permsC ->
+      unveil pathC permsC;
 #else
   -- | This library is compiled on a system which does not support
   -- @2pledge(2)@; as such, @plegg k@ does nothing.
   plegg :: String -> IO ();
   plegg _ = return ();
+
+  -- | This library is compiled on a system which does not support
+  -- @unveil(2)@; as such, @univac k@ does nothing.
+  univac :: [(String, String)] -> IO ();
+  univac = return ();
 #endif
