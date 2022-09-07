7b5a3a790a54bd33dca1166826503d313f80a4c1
Varik Valefor <varikvalefor@aol.com>
2022-09-07 17:26:51 -0400

    Some Lojban translations are added.

--- src/Plegg.hs
+++ src/Plegg.hs
@@ -8,6 +8,15 @@
 -- Maintainer  : varikvalefor@aol.com
 -- Stability   : experimental
 -- Portability : portable
+--
+-- = la .lojban.
+--
+-- ni'o le me'oi .module. cu vasru le mu'oi glibau. high-level .glibau.
+-- korbipe be lo me'oi .Haskell. proga je vu'oi la'o gy. pledge(2) .gy.
+-- je la'o gy. unveil(2) .gy. vu'o pe la'oi .OpenBSD.
+--
+-- = English
+--
 -- This module contains high-level interfaces for OpenBSD's @pledge(2)@ and
 -- @unveil(2)@.
 module Plegg where
@@ -21,7 +30,15 @@ import Data.Char;
   foreign import capi "unistd.h pledge" pledge :: CString -> CString -> IO Int;
   foreign import capi "unistd.h unveil" unveil :: CString -> CString -> IO Int;
 
-  -- | For all 'Promise' @k@, @k@ is a 'Promise' which can be passed to
+  -- | = la .lojban.
+  --
+  -- ni'o ro da poi me'oi .'Promise'. zo'u cumki fa lo nu pilno da lo
+  -- selru'e be la'oi .plegg. kei je cu se skicu ve lo mu'oi gy. man(1)
+  -- .gy. papri poi velski la'o gy. pledge(2) .gy.
+  --
+  -- = English
+  --
+  -- For all 'Promise' @k@, @k@ is a 'Promise' which can be passed to
   -- 'plegg'.  Each 'Promise' is best documented by the documentation of
   -- @pledge(2)@.
   data Promise = Stdio
@@ -59,26 +76,69 @@ import Data.Char;
                | Error
                deriving Show;
 
-  -- | @plegg k@ is roughly equivalent to C's "@pledge(k, nulll)@".
+  -- | = la .lojban.
+  --
+  -- ni'o la'o zoi. @plegg k@ .zoi. na ru'e dunli la'o zoi.
+  -- @pledge(k, null)@ pe la'oi .C.  .i ku'i drata ki'u le nu le me'oi
+  -- .'Promise'. pe la'oi .@plegg@. cu se me'oi .type.
+  --
+  -- .i lo me'oi .'Promise'. poi selru'e la'oi .@plegg@. cu binxo lo
+  -- selci'a poi selru'e la'o gy. @pledge(2)@ .gy.
+  --
+  -- = English
+  --
+  -- @plegg k@ is roughly equivalent to C's "@pledge(k, nulll)@".
   -- However, unlike the standard @pledge(2)@, @plegg@'s 'Promise's are
   -- typed.
   --
   -- 'Promise's which are passed to @plegg@ are converted into text
   -- which is passed to @pledge(2)@.
   plegg :: [Promise]
-        -- ^ The pledges which should be used
+        -- ^ = la .lojban.
+        --
+        -- zo'e poi ke'a goi ko'a zo'u cadga fa lo nu me'oi .enforce.
+        -- tu'a ko'a
+        --
+        -- = English
+        --
+        -- The pledges which should be used
         -> IO ();
   plegg k = throwErrnoIfMinus1_ "pledge" $
             withCString (promises k) $ \premises ->
             withCString "" $ \execpremises ->
             pledge premises execpremises;
 
-  -- | @promises k@ is a @pledge(2)@-suitable list of the promises
+  -- | = la .lojban.
+  --
+  -- ni'o ko'a goi la'o zoi. @promises k@ .zoi. selci'a je cu liste lo
+  -- me'oi .promise. pe la'oi @k@.  .i cumki fa lo nu ko'a selru'e la'o
+  -- gy. @pledge(2)@ .gy.
+  --
+  -- = English
+  --
+  -- @promises k@ is a @pledge(2)@-suitable list of the promises
   -- which are specified by @k@.
   promises :: [Promise] -> String;
   promises = map toLower . concat . map show;
 
-  -- | @univac@ is a high-level interface for @unveil(2)@.
+  -- | = la .lojban.
+  --
+  -- ni'o la'oi .@univac@. korbipe la'oi .Haskell. je ko'a goi la'o gy.
+  -- @unveil(2)@ .gy.
+  --
+  -- .i ro da poi .orsi re je poi se vasru la'oi .@g@. zo'u la'o zoi.
+  -- @univac g@ .zoi. pilno lo te .orsi fa'u lo ve .orsi lo pamoi sumti
+  -- be ko'a be'o fa'u lo remoi sumti be ko'a je cu co'e ko'a
+  --
+  -- .i la'o zoi. @univac [("", "")]@ .zoi. dunli la'o zoi.
+  -- @unveil(NULL, NULL)@ .zoi. pe la'oi .C.
+  --
+  -- .i le clinoi be la'o gy. @unveil(2)@ .gy. cu xamgu velski le tcila
+  -- be la'o gy. @unveil(2) .gy.
+  --
+  -- = English
+  --
+  -- @univac@ is a high-level interface for @unveil(2)@.
   --
   -- For all @(a,b)@ in @g@, @univac g@ runs C's @unveil(a, b)@.
   --
@@ -87,7 +147,14 @@ import Data.Char;
   -- @unveil(2)@'s manual page documents the specifics of this thing
   -- pretty well.
   univac :: [(FilePath, String)]
-         -- ^ [(PATH WHICH SHOULD BE EXPOSED, PERMISSIONS)]
+         -- ^ = la .lojban.
+         --
+         -- [(le me'oi .filepath. poi ke'a goi ko'a zo'u cadga fa lo nu
+         -- me'oi .expose. ko'a, lo me'oi .permission.)]
+         --
+         -- = English
+         --
+         -- [(PATH WHICH SHOULD BE EXPOSED, PERMISSIONS)]
          -> IO ();
   univac = mapM_ expose
     where
@@ -100,12 +167,26 @@ import Data.Char;
       withCString perms $ \permsC ->
       unveil pathC permsC;
 #else
-  -- | This library is compiled on a system which does not support
+  -- | = la .lojban.
+  --
+  -- ni'o pilno lo skami poi na mapti la'o gy. @pledge(2) .gy.  .i ja'e
+  -- la'o zoi. @plegg k@ .zoi. cu narplixau
+  --
+  -- = English
+  --
+  -- This library is compiled on a system which does not support
   -- @pledge(2)@; as such, @plegg k@ does nothing.
   plegg :: [Promise] -> IO ();
   plegg _ = return ();
 
-  -- | This library is compiled on a system which does not support
+  -- | = la .lojban.
+  --
+  -- ni'o pilno lo skami poi na mapti la'o gy. @unveil(2) .gy.  .i ja'e
+  -- la'o zoi. @univac k@ .zoi. cu narplixau
+  --
+  -- = English
+  --
+  -- This library is compiled on a system which does not support
   -- @unveil(2)@; as such, @univac k@ does nothing.
   univac :: [(FilePath, String)] -> IO ();
   univac _ = return ();
